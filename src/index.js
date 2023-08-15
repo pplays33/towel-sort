6ddf49a7172f839407b76328ce63bbdc6b51374f
pplays33 <pplayps98@gmail.com>
2023-08-15 17:11:10 +0300

    feat: add algo

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix === undefined){
+    return [];
+  }
+  let result = [];
+  for(let i = 0; i < matrix.length; i++){
+    result = [...result, ...( i % 2 === 0 ? matrix[i] : matrix[i].reverse() ) ];
+  }
+  return result;
 }
